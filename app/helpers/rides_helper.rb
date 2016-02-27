444ec734df5e42ee9950417ddfc4aee0d5ddab53
kmi3c <pawel.adamski85@wp.pl>
2016-02-27 18:51:46 +0100

    Providers list helper fix trailing coma

--- app/helpers/rides_helper.rb
+++ app/helpers/rides_helper.rb
@@ -1,5 +1,5 @@
 module RidesHelper
   def provider_list ids=''
-    ids.gsub(/\,$/, '').split(',').collect{|i| Ride.providers[i.to_i]}.join(',')
+    (ids.split(',')-['']).collect{|i| Ride.providers[i.to_i]}.join(',')
   end
 end
